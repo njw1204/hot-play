2cd3b214caf50719905b087b87d3964f0792279c
Jongwoo Na <njw1204@naver.com>
2019-07-29 12:26:01 +0900

    Separate the router

---
+++ router.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const validate = require("./replay-validate");
+const router = express.Router();
+
+
+router.get("/replay", function(req, res) {
+    res.status(405).render("error", {
+        error_code: res.statusCode,
+        error_summary: httpStatus[res.statusCode],
+        error_message: null
+    });
+});
+
+
+router.post("/replay", function(req, res) {
+    if (req.body.json === undefined) {
+        replay = req.body;
+    }
+    else {
+        replay = toJsonOrNull(req.body.json);
+    }
+
+    if (!(replay instanceof Object) || Object.keys(replay).length === 0) {
+        throw {
+            "type": "CustomError",
+            "statusCode": 400,
+            "msg": "Invalid JSON"
+        };
+    }
+    else {
+        validate(replay, function(err) {
+            if (err) {
+                throw {
+                    "type": "CustomError",
+                    "statusCode": 400,
+                    "msg": err
+                }
+            }
+            else {
+                res.render("index", {
+                    replay: JSON.stringify(replay, null, 0)
+                });
+            }
+        });
+    }
+});
+
+
+router.get("/", function(req, res) {
+    res.render("main");
+});
+
+
+function toJsonOrNull(str) {
+    try {
+        return JSON.parse(str);
+    } catch (e) {
+        return null;
+    }
+}
+
+
+module.exports = router;
