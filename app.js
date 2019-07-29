44dfc8dda77590e7a92b2cf707c08d12ff254168
Jongwoo Na <njw1204@naver.com>
2019-07-29 12:28:39 +0900

    Fix error and deal with invalid request in app.js

--- app.js
+++ app.js
@@ -1,33 +1,75 @@
 const express = require("express");
+const compression = require("compression");
 const helmet = require("helmet");
-const validate = require("./replay-validate");
-const fs = require("fs");
+const swaggerUi = require("swagger-ui-express");
+const logger = require("morgan");
+const httpStatus = require("http-status");
+const cors = require("cors");
+const router = require("./router");
+const swaggerDocument = require("./swagger/hotplay.json");
+
+
 const app = express();
-
-
-app.use(express.static("public"));
-app.use(helmet());
 app.set("view engine", "ejs");
 app.set("views", "./views");
+app.use(logger("short"));
+app.use(helmet());
+app.use(cors());
+app.use(compression());
+app.use(express.static("public"));
+app.use(express.json({limit: "10mb"}));
+app.use(express.urlencoded({limit: "10mb", extended: false}));
+app.use("/api", swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
+    customCss: ".swagger-ui .topbar { display: none }"
+}));
+app.use("/", router);
 
 
-app.get("/sample", function(req, res) {
-    fs.readFile("./sample_dataset/sample3.json", "utf8", function(err, data) {
-        if (err) throw err;
-        res.render("index", {
-            replay: data
-        });
+app.use(function(err, req, res, next) {
+    let msg = null;
+    res.status(400);
+
+    if (err.type === "CustomError") {
+        res.status(err.statusCode);
+        msg = err.msg;
+    }
+    else if (err.statusCode) {
+        if (err.type === "entity.parse.failed") {
+            msg = "Invalid JSON";
+        }
+        else {
+            msg = err.type;
+        }
+    }
+    else if (err.type === "entity.too.large") {
+        res.status(413);
+    }
+    else {
+        next(err);
+        return;
+    }
+
+    res.render("error", {
+        error_code: res.statusCode,
+        error_summary: httpStatus[res.statusCode],
+        error_message: msg
     });
 });
 
 
-app.get("/replay", function(req, res) {
-    validate(req.body, function(err) {
-        if (err) throw err;
-        res.render("index", {
-            replay: req.body
-        });
-    });
+app.use(function(err, req, res, next) {
+    console.log(err);
+    res.status(500);
+    res.render("error", {
+        error_code: res.statusCode,
+        error_summary: httpStatus[res.statusCode],
+        error_message: null
+    })
+});
+
+
+app.use(function(req, res, next) {
+    res.status(404).render("404");
 });
 
 
